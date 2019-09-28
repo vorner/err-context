3a9d1a5a1a885e524063864e52368a8a7ca0bc3d
Michal 'vorner' Vaner <vorner@vorner.cz>
2019-09-28 11:47:49 +0200

    Tools for examining the error chains

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::mem;
 
 pub type AnyError = Box<dyn Error + Send + Sync>;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Chain<'a>(Option<&'a (dyn Error + 'static)>);
+
+impl<'a> Iterator for Chain<'a> {
+    type Item = &'a (dyn Error + 'static);
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.0.take();
+        if let Some(current) = current {
+            self.0 = current.source();
+        }
+        current
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct DisplayChain<'a, S> {
+    chain: Chain<'a>,
+    separator: S,
+}
+
+impl<S: Display> Display for DisplayChain<'_, S> {
+    fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
+        let mut started = false;
+        // Note: iteration takes &mut, but Chain is copy, so we iterate over a copy.
+        for level in self.chain {
+            if mem::replace(&mut started, true) {
+                write!(fmt, "{}", self.separator)?;
+            }
+            write!(fmt, "{}", level)?;
+        }
+        Ok(())
+    }
+}
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
@@ -79,23 +114,69 @@ impl<M, E: ?Sized> Context<M, Wrapped<Box<E>>> {
 
 pub trait ErrorExt: Sized {
     fn context<M: Display>(self, msg: M) -> Context<M, Self>;
+    fn chain(&self) -> Chain<'_>
+    where
+        Self: 'static;
+    fn find_source<T: Error + 'static>(&self) -> Option<&T>
+    where
+        Self: 'static,
+    {
+        self.chain().find_map(|e| e.downcast_ref::<T>())
+    }
+    fn display<S: Display>(&self, separator: S) -> DisplayChain<'_, S>
+    where
+        Self: 'static,
+    {
+        DisplayChain {
+            chain: self.chain(),
+            separator,
+        }
+    }
 }
 
 impl<E: Error> ErrorExt for E {
     fn context<M: Display>(self, msg: M) -> Context<M, Self> {
         Context::new(msg, self)
     }
+    fn chain(&self) -> Chain<'_>
+    where
+        Self: 'static
+    {
+        Chain(Some(self))
+    }
 }
 
 pub trait AnyErrorExt: Sized {
     fn context<M: Display>(self, msg: M) -> Context<M, Wrapped<Self>>;
-}
-
-impl<E: Error + ?Sized> AnyErrorExt for Box<E> {
-    fn context<M: Display>(self, msg: M) -> Context<M, Wrapped<Box<E>>> {
-        Context::from_boxed(msg, self)
+    fn chain(&self) -> Chain<'_>;
+    fn find_source<T: Error + 'static>(&self) -> Option<&T> {
+        self.chain().find_map(|e| e.downcast_ref::<T>())
+    }
+    fn display<S: Display>(&self, separator: S) -> DisplayChain<'_, S> {
+        DisplayChain {
+            chain: self.chain(),
+            separator,
+        }
     }
 }
+
+macro_rules! impl_any_error {
+    ($ty: ty) => {
+        impl AnyErrorExt for Box<$ty> {
+            fn context<M: Display>(self, msg: M) -> Context<M, Wrapped<Box<$ty>>> {
+                Context::from_boxed(msg, self)
+            }
+            fn chain(&self) -> Chain<'_> {
+                Chain(Some(&**self))
+            }
+        }
+    }
+}
+
+impl_any_error!(dyn Error + Send + Sync);
+impl_any_error!(dyn Error + Send);
+impl_any_error!(dyn Error + Sync);
+impl_any_error!(dyn Error);
 
 pub trait ResultExt<T, E>: Sized {
     fn context<M>(self, msg: M) -> Result<T, Context<M, E>>
@@ -180,6 +261,8 @@ mod tests {
     use std::io::{Error as IoError, Read};
     use super::*;
 
+    // Some tests that try stuff compiles, that all the traits are implemented for everything they
+    // should be.
     fn _wrapped_any_error() -> impl Error {
         let e: AnyError = IoError::last_os_error().into();
         Wrapped(e)
@@ -203,5 +286,40 @@ mod tests {
     fn _double_context() -> Result<(), AnyError> {
         _context_result().with_context(|e| format!("Hey: {}", e))?;
         Ok(())
+    }
+
+    #[derive(Copy, Clone, Debug)]
+    struct Dummy;
+
+    impl Display for Dummy {
+        fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
+            write!(fmt, "Dummy error!")
+        }
+    }
+
+    impl Error for Dummy {}
+
+    fn get_chain() -> AnyError {
+        Dummy.context("Sorry").into()
+    }
+
+    #[test]
+    fn iter_chain() {
+        assert_eq!(1, Dummy.chain().count());
+        assert_eq!(2, get_chain().chain().count());
+    }
+
+    #[test]
+    fn find_dummy() {
+        assert!(Dummy.find_source::<Dummy>().is_some());
+        assert!(get_chain().find_source::<Dummy>().is_some());
+        assert!(get_chain().find_source::<IoError>().is_none());
+    }
+
+    #[test]
+    fn display_chain() {
+        let chain = get_chain();
+        assert_eq!("Sorry", chain.to_string());
+        assert_eq!("Sorry: Dummy error!", chain.display(": ").to_string());
     }
 }
